f0cf947d0b2abecdf796a2a21dce7576375e71d6
Alejandra Rubalcava <arubalcava@group.on>
2018-06-20 13:51:32 -0500

    added index.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //require npm packages
 var express = require("express");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
 var mongoose = require("mongoose");
 
 //require scraping tools
+var axios = require("axios");
 var cheerio = require("cheerio");
 
 //require models
@@ -34,11 +36,11 @@ mongoose.connect(MONGODB_URI);
 // A GET route for scraping the website
 app.get("/scrape", function (req, res) {
     //get body of html
-    axios.get("").then(function (response) {
+    axios.get("https://www.cnn.com/").then(function (response) {
         // load body of html to cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
         //grab specific information by tag
-        $("").each(function (i, element) {
+        $("article h2").each(function (i, element) {
             var result = {};
 
             // Add the text and href of every link, and save them as properties of the result object
@@ -75,37 +77,37 @@ app.get("/articles", function (req, res) {
 });
 
 //A GET route for getting all saved articles from the DB
-app.get("/articles", function (req, res) {
-    db.Article.find({})
-    .then(function (dbArticle) {
-        res.json(dbArticle)
-    })
-    .catch(function (err) {
-        res.json(err);
-    });
-});
+// app.get("/articles", function (req, res) {
+//     db.Article.find({})
+//     .then(function (dbArticle) {
+//         res.json(dbArticle)
+//     })
+//     .catch(function (err) {
+//         res.json(err);
+//     });
+// });
 
 //A POST route for saving articles
 
 //A POST route for saving/updating an article's associated notes
-app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-      .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+// app.post("/articles/:id", function(req, res) {
+//     // Create a new note and pass the req.body to the entry
+//     db.Note.create(req.body)
+//       .then(function(dbNote) {
+//         // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+//         // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//         // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//         return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//       })
+//       .then(function(dbArticle) {
+//         // If we were able to successfully update an Article, send it back to the client
+//         res.json(dbArticle);
+//       })
+//       .catch(function(err) {
+//         // If an error occurred, send it to the client
+//         res.json(err);
+//       });
+//   });
   
 //start the server
 app.listen(PORT, function () {
